4ee9d21d25180c8c6cba69128f550a6b4a499100
Awais Ali <awaisali786378@gmail.com>
2024-03-14 05:13:43 -0700

    changes

--- backend/models/productPicture.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const pictureSchema = new mongoose.Schema({
-  pic: {
-    filename: String,
-    url: String,
-  },
-});
-export default mongoose.model("File", pictureSchema);
